6ab0168d1e3a6dbdd5525a2fe68457b7838b5434
Ricardo <rr155914@gmail.com>
2021-07-15 09:23:38 -0500

    Add list structure

--- webpack.config.js
+++ webpack.config.js
@@ -5,12 +5,21 @@ module.exports = {
   entry: './src/index.js',
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html',
+      template: './src/index.html'
     }),
+
   ],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
   },
 };
