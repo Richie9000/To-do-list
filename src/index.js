acee2e0c2a3abbfb63ca61682c016add8faba933
Ricardo <rr155914@gmail.com>
2021-07-17 17:38:49 -0500

    Check linters

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import createList from './modules/frontend/listItems';
 import { loadTasks } from './modules/backend/task';
 
 const run = () => {
-createList(loadTasks());
+  createList(loadTasks());
 };
 
 run();
